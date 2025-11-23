2ced7522b5c02f03710fe9b1b71ccaddaf5d88a4
Florian Tranier <tranierflorian@pm.me>
2025-11-23 21:07:29 +0100

    revert

--- config/logger.ts
+++ config/logger.ts
@@ -17,7 +17,7 @@ const loggerConfig = defineConfig({
       transport: {
         targets: targets()
           .pushIf(!app.inProduction, targets.pretty())
-          .pushIf(app.inProduction, targets.pretty())
+          .pushIf(app.inProduction, targets.file({ destination: 1 }))
           .toArray(),
       },
     },
